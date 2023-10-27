4fc475ec80c2d8691d44b909ffa2aed825de8dbe
jorLuan <99776944+jorLuan@users.noreply.github.com>
2023-10-27 10:57:16 -0500

    ccrear-editar-mostrar_users

--- src/pages/view-table-users/view-table-users.tsx
+++ src/pages/view-table-users/view-table-users.tsx
@@ -1,22 +1,41 @@
+"use client"
+import { Card, CardHeader, CardBody, Button,Table,Select,Input} from '@chakra-ui/react'
+import { FaEdit, FaUserPlus } from 'react-icons/fa'; 
+import FormModal from '@/pages/create-user/form-crate-model';
+import FormEditModal from '../create-user/fom-edit-modal';
 
-import { Card, CardHeader, CardBody, Button,Table,Select} from '@chakra-ui/react'
-import { FaEdit } from 'react-icons/fa'; 
+
+function ViewTableUsers(this: any) {
 
 
-
-function ViewTableUsers() {
-   
-    const stateOptions = ['Habilitado', 'Inhabilitado'];
     const users = [
-        { id: 104619011256, name: 'Jorge Herley Luligo',usuario:'jluligo19', email: 'jluligo@example.com' },
-        { id: 104619014589, name: 'Julia Sofia Garcia',usuario: 'jsgarcia19', email: 'jsgarcia@example.com' },
+        { codigo: 104619011256, nombre: 'Jorge Herley Luligo',usuario:'jluligo19', correo: 'jluligo@example.com'},
+        { codigo: 104619014589, nombre: 'Julia Sofia Garcia',usuario: 'jsgarcia19', correo: 'jsgarcia@example.com' },
         
     ];
 
+    const [FormModalComponent, openModal, closeModal] = FormModal()
+    const [FormModalEditComponent, openEditModal, closeEditModal] = FormEditModal()
+    const stateOptions = ['Habilitado', 'Inhabilitado'];
+
+
+
     return (
+        
+        <div>
+        <h1 style={{ marginBottom: '20px' }}><b>Usuarios</b></h1>
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+            <Input placeholder='Buscar' />
+            <Button leftIcon={< FaUserPlus />} colorScheme="blue" size="md" marginLeft="5" onClick={openModal}></Button>
+            <FormModalComponent />
+            
+        </div >
+            
         <Card textAlign="center">
+        
+         
             <CardHeader>
-                <h2>Tabla de Usuarios</h2>
+                <h2></h2>
             </CardHeader>
 
             <CardBody>
@@ -33,11 +52,11 @@ function ViewTableUsers() {
                     </thead>
                     <tbody>
                         {users.map((user) => (
-                            <tr key={user.id}>
-                                <td>{user.id}</td>
-                                <td>{user.name}</td>
+                            <tr key={user.codigo}>
+                                <td>{user.codigo}</td>
+                                <td>{user.nombre}</td>
                                 <td>{user.usuario}</td>
-                                <td>{user.email}</td>
+                                <td>{user.correo}</td>
                                 <td><Select>
                                         {stateOptions.map((option) => (
                                             <option key={option} value={option}>
@@ -48,18 +67,20 @@ function ViewTableUsers() {
                                     
                                 </td>
                                 <td> 
-                                <Button leftIcon={<FaEdit />} colorScheme="blue" size="sm">
-                                        Editar
-                                    </Button>
+                                <Button rightIcon={<FaEdit />} colorScheme="blue" size="md" onClick={openEditModal}>
+                                    <FormModalEditComponent />
+                                </Button>
                                         
-                                       </td>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
                 </Table>
             </CardBody>
         </Card>
+        </div>
     );
   }
   export default ViewTableUsers
+  
   

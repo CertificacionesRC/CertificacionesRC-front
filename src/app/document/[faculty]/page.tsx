245192c8bc49570571f4775baea0161687841540
Andres990726 <jagarciag@unicauca.edu.co>
2023-10-30 22:56:10 -0500

    [FEAT] name indexs

--- src/app/document/[faculty]/page.tsx
+++ src/app/document/[faculty]/page.tsx
@@ -12,55 +12,55 @@ function ItemPage() {
       <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginTop: '20px' }}>
         <ItemsComponent
           nameSubItems={['1. Subindice ', '2. Subindice ', '3. Subindice ']}
-          nameItem="1. Indice"
+          nameItem="1. Denominación del programa"
           pathname={pathname}
         />
 
         <ItemsComponent
           nameSubItems={['1. Subindice ', '2. Subindice ', '3. Subindice ']}
-          nameItem="2. Indice"
+          nameItem="2. Justificación del programa"
           pathname={pathname}
         />
 
         <ItemsComponent
           nameSubItems={['1. Subindice ', '2. Subindice ', '3. Subindice ']}
-          nameItem="3. Indice"
+          nameItem="3. Aspectos curriculares"
           pathname={pathname}
         />
 
         <ItemsComponent
           nameSubItems={['1. Subindice ', '2. Subindice ', '3. Subindice ']}
-          nameItem="4. Indice"
+          nameItem="4. Organización de actividades académicas y proceso formativo"
           pathname={pathname}
         />
 
         <ItemsComponent
           nameSubItems={['1. Subindice ', '2. Subindice ', '3. Subindice ']}
-          nameItem="5. Indice"
+          nameItem="5. Investigación, innovación y/o creación artística y cultural"
           pathname={pathname}
         />
 
         <ItemsComponent
           nameSubItems={['1. Subindice ', '2. Subindice ', '3. Subindice ']}
-          nameItem="6. Indice"
+          nameItem="6. Relación con el sector externo"
           pathname={pathname}
         />
 
         <ItemsComponent
           nameSubItems={['1. Subindice ', '2. Subindice ', '3. Subindice ']}
-          nameItem="7. Indice"
+          nameItem="7. Profesores"
           pathname={pathname}
         />
 
         <ItemsComponent
           nameSubItems={['1. Subindice ', '2. Subindice ', '3. Subindice ']}
-          nameItem="8. Indice"
+          nameItem="8. Medios educativos"
           pathname={pathname}
         />
 
         <ItemsComponent
           nameSubItems={['1. Subindice ', '2. Subindice ', '3. Subindice ']}
-          nameItem="9. Indice"
+          nameItem="9. Infraestructura física y tecnológica"
           pathname={pathname}
         />
       </div>

7f4e10e46d9152dc318b8ce3dfd64aa9b2d29e0b
Andres990726 <jagarciag@unicauca.edu.co>
2023-11-21 23:48:56 -0500

    [FEAT] button generar documento

--- src/app/home/[faculty]/page.tsx
+++ src/app/home/[faculty]/page.tsx
@@ -1,12 +1,17 @@
-import { Stack, Text } from '@chakra-ui/react'
+import { Button, Flex, Spacer, Stack, Text } from '@chakra-ui/react'
 import Items from './components/items'
 
 function DocumentFacultyPage() {
   return (
     <Stack spacing="8">
-      <Text fontWeight="semibold" fontSize="2xl" color="textColor">
-        Condiciones de calidad programa de arquitectura
-      </Text>
+      <Flex>
+        <Text fontWeight="semibold" fontSize="2xl" color="textColor">
+          Condiciones de calidad programa de arquitectura
+        </Text>
+        <Spacer />
+        <Button variant="solid">Generar documento</Button>
+      </Flex>
+
       <Items />
     </Stack>
   )

a369bbc4f2cf46e0a128bdf61d47ceb15904f820
Andres990726 <jagarciag@unicauca.edu.co>
2023-11-23 00:09:36 -0500

    [FEAT] view pdf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['img.freepik.com', 'veterinaire-tour-hassan.com'],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use: 'raw-loader',
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,6 +2,7 @@ export const ROUTES = {
   DOCUMENT_FACULTY: (faculty: string) => `/home/${faculty}`,
   DOCUMENT_SUBITEM: (id: string) => `/home/subitem/${id}`,
   DOCUMENT_ITEM: (id: string) => `/home/item/${id}`,
+  DOCUMENT_PDF: (name: string) => `/home/view/${name}`,
   START_DOCUMENT: '/home/start',
   DOCUMENT: '/document',
   HISTORY: '/history',
